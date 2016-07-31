6e4f9a00bf87e75c4464ddcaa205d8ea2aed9cb9
Chris Kemp <chris.kemp3000@gmail.com>
2016-07-31 11:51:51 +0100

    Created basic game

---
+++ Assets/scripts/character/movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float Speed = 20f;
+	public float Rotation = 90f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Process Inputs
+		var left = Input.GetKey("a") ? 1 : 0;
+		var right = Input.GetKey("d") ? 1 : 0;
+		var forward = Input.GetKey("w") ? 1 : 0;
+		var backward = Input.GetKey("s") ? 1 : 0;
+
+		// Turn inputs into direction
+		var direction	= forward - backward;
+		var turn 		= (right - left) * direction; // only turn when moving
+		
+		var body = GetComponent<Rigidbody>();
+
+		// Turn it
+		if (turn != 0) {
+			var rot = new Vector3(0, Rotation, 0);
+			var deltaRotation = Quaternion.Euler(rot * turn * Time.deltaTime);
+			body.MoveRotation(body.rotation * deltaRotation);
+		}
+
+		// Move it
+		if (direction != 0) {
+			body.MovePosition(transform.position + transform.forward * (direction * Speed * Time.deltaTime));
+		}
+	}
+}
